6e798675047ef7b50ec291d924c185dc1f4750a0
Prymordial_Soupe <69216104+Dboi6339-Soupe@users.noreply.github.com>
2021-08-09 17:30:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// init "x"
+var x = Math.floor((Math.random() * 10) + 1);
+
+// init "y"
+var y = Math.floor((Math.random() * 10) + 1);
+
+// init equation
+const equation = Math.pow(x, y);
+
+// log equation
+console.log(equation);
