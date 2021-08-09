a9d770e8fe537fc8b153faae725d0bf192186a42
Prymordial_Soupe <69216104+Dboi6339-Soupe@users.noreply.github.com>
2021-08-09 17:49:39 +0000

    adjusted comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// adjust the parameters "between one and ???"
+// adjust the parameters "between 'Number' and 'Number'"
 var xParameter1 = 10
-const xParameter2 = 1 // must be one, change at your discretion
+const xParameter2 = 1 // default is one, change at your discretion
 var yParameter1 = 10
-const yParameter2 = 1  // must be one, change at your discretion
+const yParameter2 = 1  // default is one, change at your discretion
 
 // init "x"
 var x = Math.floor((Math.random() * xParameter1) + xParameter2);
