1758af14b84cfbfa47ef74b7db0248356f71d381
Prymordial_Soupe <69216104+Dboi6339-Soupe@users.noreply.github.com>
2021-08-09 17:37:13 +0000

    added adjustable parameters

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// adjust the parameters
+var xParameter1 = 10
+var xParameter2 = 1
+var yParameter1 = 10
+var yParameter2 = 1
+
 // init "x"
-var x = Math.floor((Math.random() * 10) + 1);
+var x = Math.floor((Math.random() * xParameter1) + xParameter2);
 
 // init "y"
-var y = Math.floor((Math.random() * 10) + 1);
+var y = Math.floor((Math.random() * yParameter1) + yParameter2);
 
 // init equation
 const equation = Math.pow(x, y);
